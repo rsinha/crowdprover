cf9b2b6645c31cd11c987c41976c3935d214d979
Rohit Sinha <sinharo@gmail.com>
2013-02-26 10:31:58 -0800

    added main page

--- proveit/urls.py
+++ proveit/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
+from proveit import views
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', views.index, name='index'),
     url(r'^polls/', include('proveit.polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
