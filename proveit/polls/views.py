a2bb82e64dce387f3d2e8cfe46e4c2c8ab5b9c3a
Rohit Sinha <sinharo@gmail.com>
2013-02-26 11:33:31 -0800

    added poll templates

--- proveit/polls/views.py
+++ proveit/polls/views.py
@@ -3,8 +3,11 @@ from proveit.polls.models import Poll
 
 def index(request):
     latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-    output = ', '.join([p.question for p in latest_poll_list])
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = Context({
+        'latest_poll_list': latest_poll_list,
+    })
+    return HttpResponse(template.render(context))
 
 def detail(request, poll_id):
     return HttpResponse("You're looking at poll %s." % poll_id)
