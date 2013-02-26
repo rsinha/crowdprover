b971c0bb3c68625f5428f4620df63b87c0b2c601
Rohit Sinha <sinharo@gmail.com>
2013-02-26 11:39:05 -0800

    render shortcut

--- proveit/polls/views.py
+++ proveit/polls/views.py
@@ -1,15 +1,17 @@
 from django.http import HttpResponse
 from django.template import Context, loader
+from django.shortcuts import render
 
 from proveit.polls.models import Poll
 
 def index(request):
     latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
+    #template = loader.get_template('polls/index.html')
     context = Context({
         'latest_poll_list': latest_poll_list,
     })
-    return HttpResponse(template.render(context))
+    #return HttpResponse(template.render(context))
+    return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
     return HttpResponse("You're looking at poll %s." % poll_id)
