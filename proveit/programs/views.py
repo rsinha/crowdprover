c481a1931949af0d37277c2d234b45f620c35794
Rohit Sinha <sinharo@gmail.com>
2013-02-27 11:21:31 -0800

    missing import

--- proveit/programs/views.py
+++ proveit/programs/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.template import Context, loader
 from django.shortcuts import get_object_or_404, render
+from django.utils import timezone
 
 from proveit.programs.models import Program, Invariant
 
