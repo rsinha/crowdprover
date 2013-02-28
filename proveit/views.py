948d3fd7990215bc58db14ba2cf01963f3fa9c51
Rohit Sinha <sinharo@gmail.com>
2013-02-28 09:42:48 -0800

    removing polls

--- proveit/views.py
+++ proveit/views.py
@@ -2,6 +2,6 @@ from django.http import HttpResponse
 from django.template import Context, loader
 
 def index(request):
-    template = loader.get_template('mainpage.html')
+    template = loader.get_template('main/mainpage.html')
     context = Context({})
     return HttpResponse(template.render(context))
