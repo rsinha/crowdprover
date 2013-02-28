d5d4d704ee8fc6b01fe2632447dfd251c3748a3b
Rohit Sinha <sinharo@gmail.com>
2013-02-27 18:29:01 -0800

    revert to rendering results

--- proveit/views.py
+++ proveit/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("You are at the main page. Check out our <a href="/programs">programs</a>")
+    return HttpResponse("<html><body>You are at the main page. Check out our <a href="/programs">programs</a></body></html>")
