9ba06047acbef2932f417e3db6878f0f73c2062a
Rohit Sinha <sinharo@gmail.com>
2013-02-28 16:17:17 -0800

    try

--- proveit/programs/views.py
+++ proveit/programs/views.py
@@ -30,30 +30,23 @@ def results(request, program_id):
 def submit(request, program_id):
     program = get_object_or_404(Program, pk=program_id)
     code = open(program.source, 'r').read() 
-    try:
-        author = request.POST['author']
-        content = request.POST['content']
-        line = request.POST['line']
-        date = timezone.now()
-    except (KeyError):
+    trace = [{'alias': 'x', 'values': ['1', '2', '3', '4', '5', '6', '7', '8', '9'], 'name': 'x'}, {'alias': 'n_data', 'values': ['1', '2', '3', '4', '5', '6', '7', '8', '9'], 'name': 'n->data'},{'alias': 'n_next', 'values': ["0xabcd", "0xabcd", "0xbacd", "0xbeee", "0xabcd", "0xbecf", "0xbcef", "0xabcd", "0xdead", "0xabcd", "0xabcd", "0xabcd"], 'name': 'n->next'}]
+    author = request.POST['author']
+    content = request.POST['content']
+    line = request.POST['line']
+    date = timezone.now()
+    # Always return an HttpResponseRedirect after successfully dealing
+    # with POST data. This prevents data from being posted twice if a
+    # user hits the Back button.
+    #return HttpResponseRedirect(reverse('programs:results', args=(program.id,)))
+    if author == "" or content == "" or line == "": 
         # Redisplay the program voting form.
         return render(request, 'programs/detail.html', {
             'program': program,
 	    'code': code,
-            'error_message': "Something went wrong. Contact the webninja.",
+	    'trace': trace,
+            'error_message': "Please fill all fields.",
         })
     else:
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        #return HttpResponseRedirect(reverse('programs:results', args=(program.id,)))
-       	if author == "" or content == "" or line == "": 
-  	    # Redisplay the program voting form.
-            return render(request, 'programs/detail.html', {
-                'program': program,
-	        'code': code,
-                'error_message': "Please fill all fields.",
-            })
-	else:
-	    program.invariant_set.create(author=author, content=content, line=int(line), date=date)
+        program.invariant_set.create(author=author, content=content, line=int(line), date=date)
             return render(request, 'programs/results.html', {'program': program, 'code': code})
