066ad9553d1aaf64fb9be9c5b72d7675ac205158
Rohit Sinha <sinharo@gmail.com>
2013-03-07 12:13:50 -0800

    look n feel

--- proveit/settings.py
+++ proveit/settings.py
@@ -172,16 +172,16 @@ LOGGING = {
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-#DATABASES['default'] =  dj_database_url.config()
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'proveit',
-        'USER': 'crowdprover',
-        'PASSWORD': 'crowdprover',
-        'HOST': ''
-    }
-}
+DATABASES['default'] =  dj_database_url.config()
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'proveit',
+#        'USER': 'crowdprover',
+#        'PASSWORD': 'crowdprover',
+#        'HOST': ''
+#    }
+#}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
